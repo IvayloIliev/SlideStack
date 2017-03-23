6326ec51b9affc3504245378854a7b2602da230d
vntodorova <veneta95@gmail.com>
2017-03-23 13:33:18 +0200

    SlideCell

---
+++ SlideStack/SlideStack/Defines.h
@@ -0,0 +1,13 @@
+//
+//  Defines.h
+//  SlideStack
+//
+//  Created by Nemetschek A-Team on 3/23/17.
+//  Copyright © 2017 Nemetschek. All rights reserved.
+//
+
+#ifndef Defines_h
+#define Defines_h
+
+
+#endif /* Defines_h */
---
+++ SlideStack/SlideStack/SlideCell.m
@@ -0,0 +1,21 @@
+//
+//  SlideCell.m
+//  SlideStack
+//
+//  Created by Nemetschek A-Team on 3/23/17.
+//  Copyright © 2017 Nemetschek. All rights reserved.
+//
+
+#import "SlideCell.h"
+
+@implementation SlideCell
+
+/*
+// Only override drawRect: if you perform custom drawing.
+// An empty implementation adversely affects performance during animation.
+- (void)drawRect:(CGRect)rect {
+    // Drawing code
+}
+*/
+
+@end
